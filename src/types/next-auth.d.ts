c257c6a100043f5de6a2636634a71fa90c83a519
smyn95 <tlsalduszz@gmail.com>
2024-01-07 15:31:43 +0900

    next-auth에 username 타입 명시

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,9 @@
+import nextAuth, {DefaultSession} from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      username: string;
+    } & DefaultSession["user"];
+  }
+}
