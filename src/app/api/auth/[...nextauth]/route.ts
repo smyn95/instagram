e4ec9df7604b4d6e82c3aec26e09ae0a776989b8
smyn95 <tlsalduszz@gmail.com>
2024-01-07 14:46:27 +0900

    callbackUrl 이전 페이지로 돌아오게 파라미터 추가

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,8 @@
-import NextAuth from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
+import { authOptions } from "@/lib/auth";
+import NextAuth from "next-auth/next";
 
 
-const handler = NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.NEXT_PUBLIC_GOOGLE_OAUTH_ID || '',
-      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_OAUTH_SECRET || '',
-    }),
-  ],
-});
+const handler = NextAuth(authOptions);
 
 
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST }
---
+++ src/app/api/auth/signin/page.tsx
@@ -0,0 +1,26 @@
+import Signin from "@/components/Signin";
+import { authOptions } from "@/lib/auth"
+import { getServerSession } from "next-auth"
+import { getProviders } from "next-auth/react";
+import { redirect } from "next/navigation";
+
+type Props = {
+  searchParams : {
+    callbackUrl : string
+  }
+}
+
+export default async function SigninPage ({searchParams : {callbackUrl}} : Props) {
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+    redirect('/');
+  }
+
+  const providers = (await getProviders()) ?? {};
+    return (
+      <section className="flex justify-center mt-[30%]">
+        <Signin providers={providers} callbackUrl={callbackUrl ?? '/'}/>
+        </section>
+    )
+}
