db5061eb44bace897aac15299bc820af552c4e94
smyn95 <tlsalduszz@gmail.com>
2024-01-08 21:09:20 +0900

    옵셔널 타입이 아닌 필수 전역 타입으로 변경

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,9 +1,7 @@
-import nextAuth, { DefaultSession } from "next-auth";
+import { User } from "@/model/user";
 
 declare module "next-auth" {
   interface Session {
-    user: {
-      username: string;
-    } & DefaultSession["user"];
+    user: User;
   }
 }
