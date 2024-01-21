c5a36a70bf317d24d44093e66ac2392acdcfd8ac
smyn95 <tlsalduszz@gmail.com>
2024-01-21 16:19:21 +0900

    검색어 리스트 컴포넌트 추가 및 타입 정의

--- src/components/UserSearch.tsx
+++ src/components/UserSearch.tsx
@@ -1,12 +1,48 @@
 "use client";
 
-import { useState } from "react";
+import { ProfileUser } from "@/model/user";
+import { FormEvent, useState } from "react";
 import useSWR from "swr";
+import GridSpinner from "./ui/GridSpinner";
+import UserCard from "./UserCard";
 
 export default function UserSearch() {
   const [keyword, setKeyword] = useState("");
-  const { data, isLoading, error } = useSWR(`/api/search/${keyword}`);
-  console.log(data);
+  const {
+    data: users,
+    isLoading,
+    error,
+  } = useSWR<ProfileUser[]>(`/api/search/${keyword}`);
 
-  return <></>;
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+  };
+
+  return (
+    <section className="w-full max-w-2xl my-4 flex flex-col items-center">
+      <form className="w-full mb-4" onSubmit={onSubmit}>
+        <input
+          className="w-full text-xl p-3 outline-none border border-gray-400"
+          type="text"
+          autoFocus
+          placeholder="Search for a username or name"
+          value={keyword}
+          onChange={(e) => setKeyword(e.target.value)}
+        />
+      </form>
+      {error && <p>무언가가 잘못 되었음 😜</p>}
+      {isLoading && <GridSpinner />}
+      {!isLoading && !error && users?.length === 0 && (
+        <p>찾는 사용자가 없음 😭</p>
+      )}
+      <ul className="w-full p-4">
+        {users &&
+          users.map((user) => (
+            <li key={user.username}>
+              <UserCard user={user} />
+            </li>
+          ))}
+      </ul>
+    </section>
+  );
 }
--- src/service/user.ts
+++ src/service/user.ts
@@ -1,3 +1,4 @@
+import { ProfileUser } from "@/model/user";
 import { client } from "./sanity";
 
 type OAuthUser = {
@@ -30,18 +31,26 @@ export async function getUserByUsername(username: string) {
       following[]->{username,image},
       followers[]->{username,image},
       "bookmarks":bookmarks[]->_id
-    }`
+    }`,
   );
 }
 export async function searchUsers(keyword?: string) {
   const query = keyword
     ? `&& (name match "${keyword}") || (username match "${keyword}")`
     : "";
-  return client.fetch(
-    `*[_type =="user" ${query}]{
+  return client
+    .fetch(
+      `*[_type =="user" ${query}]{
       ...,
       "following": count(following),
       "followers": count(followers),
-    }`
-  );
+    }`,
+    )
+    .then((users) =>
+      users.map((user: ProfileUser) => ({
+        ...user,
+        following: user.following ?? 0,
+        followers: user.followers ?? 0,
+      })),
+    );
 }
