ded39eec3b6bb910c34f73ae21a871075c44b20f
smyn95 <tlsalduszz@gmail.com>
2024-03-17 14:34:49 +0900

    유저의 카테고리별 포스트 불러오기 api 추가

--- src/service/post.ts
+++ src/service/post.ts
@@ -19,11 +19,9 @@ export async function getFollowingPostsOf(username: string) {
       `*[_type == "post" && author->username == "${username}" 
   || author._ref in *[_type == "user" && username == "${username}"].following[]._ref]
   | order(_createdAt desc) {${simplePostProjection}}
-  `,
+  `
     )
-    .then((posts) =>
-      posts.map((post: SimplePost) => ({ ...post, image: urlFor(post.image) })),
-    );
+    .then(mapPosts);
 }
 
 export async function getPost(id: string) {
@@ -38,7 +36,45 @@ export async function getPost(id: string) {
       comments[]{comment, "username": author->username, "image": author->image},
       "id":_id,
       "createdAt":_creatdAt
-    }`,
+    }`
     )
     .then((post) => ({ ...post, image: urlFor(post.image) }));
+}
+
+export async function getPostsOf(username: string) {
+  return client
+    .fetch(
+      `*[_type == "post" && author->username == "${username}"]
+      | order(_createdAt desc){
+        ${simplePostProjection}
+      }`
+    )
+    .then(mapPosts);
+}
+
+export async function getLikedPostsOf(username: string) {
+  return client
+    .fetch(
+      `*[_type == "post" && "${username}" in likes[]->username]
+      | order(_createdAt desc){
+        ${simplePostProjection}
+      }`
+    )
+    .then(mapPosts);
+}
+export async function getSavedPostsOf(username: string) {
+  return client
+    .fetch(
+      `*[_type == "post" && _id in *[_type=="user" && username=="${username}"].bookmarks[]._ref]
+      | order(_createdAt desc){
+        ${simplePostProjection}
+      }`
+    )
+    .then(mapPosts);
+}
+function mapPosts(posts: SimplePost[]) {
+  return posts.map((post: SimplePost) => ({
+    ...post,
+    image: urlFor(post.image),
+  }));
 }
