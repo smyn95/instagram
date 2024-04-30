dea0b0cd0641ad1924771c5dc8c6653591c0afdb
smyn95 <tlsalduszz@gmail.com>
2024-04-30 19:53:21 +0900

    metadata 추가
    중복 함수 cache 처리

--- src/app/api/auth/signin/page.tsx
+++ src/app/api/auth/signin/page.tsx
@@ -1,8 +1,14 @@
 import Signin from "@/components/Signin";
 import { authOptions } from "@/lib/auth";
+import { Metadata } from "next";
 import { getServerSession } from "next-auth";
 import { getProviders } from "next-auth/react";
 import { redirect } from "next/navigation";
+
+export const metadata: Metadata = {
+  title: "signin",
+  description: "Signup or login to your account.",
+};
 
 type Props = {
   searchParams: {
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,4 +1,10 @@
 import UserSearch from "@/components/UserSearch";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "User Search",
+  description: "Search users to follow",
+};
 
 export default function SearchPage() {
   return (
