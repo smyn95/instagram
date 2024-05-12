78ea44d63726389c94c3cde13bc570e0969616b1
smyn95 <tlsalduszz@gmail.com>
2024-05-12 15:34:50 +0900

    게시물 추가 페이지에서 drag & drop으로 이미지를 올릴 수 있다.

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,7 +1,20 @@
-export default function NewPostPage() {
-  return (
-    <>
-      <div>NewPostPage</div>
-    </>
-  );
+import NewPost from "@/components/NewPost";
+import { authOptions } from "@/lib/auth";
+import { Metadata } from "next";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
+export const metadata: Metadata = {
+  title: "New Post",
+  description: "Create a new post",
+};
+
+export default async function NewPostPage() {
+  const session = await getServerSession(authOptions);
+
+  if (!session?.user) {
+    redirect("/auth/signin");
+  }
+
+  return <NewPost user={session.user} />;
 }
