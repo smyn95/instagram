58eb1bef3a255e108e51ce79d8b53ba32f1296bb
smyn95 <tlsalduszz@gmail.com>
2024-05-12 13:55:47 +0900

    useTransition 활용하여 팔로우 기능 개선

--- src/components/FollowButton.tsx
+++ src/components/FollowButton.tsx
@@ -1,12 +1,21 @@
 "use client";
+
 import useMe from "@/hooks/me";
 import { ProfileUser } from "@/model/user";
 import Button from "./ui/Button";
+import { useRouter } from "next/navigation";
+import { useState, useTransition } from "react";
+import { PulseLoader } from "react-spinners";
 
 type Props = {
   user: ProfileUser;
 };
 export default function FollowButton({ user }: Props) {
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  const [isFetching, setIsFetching] = useState(false);
+  const isUpdating = isPending || isFetching;
+
   const { username } = user;
   const { user: loggedInUser, toggleFollow } = useMe();
 
@@ -17,14 +26,31 @@ export default function FollowButton({ user }: Props) {
 
   const text = following ? "Unfollow" : "Follow";
 
-  const handleFollow = () => {
-    toggleFollow(user.id, !following);
+  const handleFollow = async () => {
+    setIsFetching(true);
+    await toggleFollow(user.id, !following);
+    setIsFetching(false);
+
+    startTransition(() => {
+      router.refresh();
+    });
   };
 
   return (
     <>
       {showButton && (
-        <Button text={text} onClick={handleFollow} red={text === "Unfollow"} />
+        <div>
+          {isUpdating && (
+            <div>
+              <PulseLoader size={6} />
+            </div>
+          )}
+          <Button
+            text={text}
+            onClick={handleFollow}
+            red={text === "Unfollow"}
+          />
+        </div>
       )}
     </>
   );
